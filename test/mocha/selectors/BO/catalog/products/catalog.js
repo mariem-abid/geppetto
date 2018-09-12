19806d5e35d7f5c7610c19f5da76911949e3b900
mariem-abid <marieem.abid@gmail.com>
2018-09-12 14:26:33 +0100

    TE:PR-10375

--- test/mocha/selectors/BO/catalog/products/catalog.js
+++ test/mocha/selectors/BO/catalog/products/catalog.js
@@ -1,5 +1,9 @@
 module.exports = {
   Catalog: {
     add_new_button: '#page-header-desc-configuration-add',
+    filters_by_categories_button: '#product_catalog_category_tree_filter > button',
+    expand_action: '#product_catalog_category_tree_filter_expand',
+    collapse_action: '#product_catalog_category_tree_filter_collapse',
+    check_tree: '#product_categories_categories > ul > li'
   }
 };
